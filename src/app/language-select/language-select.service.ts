c7743a1a60bd075e81f6bc8724980b50ebc84ce8
Johannes Walter <jwalter@ALLEGIANCE.LOCAL>
2017-03-26 14:28:42 -0600

    feat(StudyKana) Can now iterate through items

--- src/app/language-select/language-select.service.ts
+++ src/app/language-select/language-select.service.ts
@@ -7,16 +7,20 @@ export class LanguageSelectService {
   // Observable sources
   private hiragana = new Subject<CharacterModel[]>();
   private katakana = new Subject<CharacterModel[]>();
+  private characters = new Subject<CharacterModel[]>();
 
   // Observable streams
   hiragana$ = this.hiragana.asObservable();
   katakana$ = this.katakana.asObservable();
+  characters$ = this.characters.asObservable();
 
   // Service message commands
   updateHiragana(characters: CharacterModel[]) {
     this.hiragana.next(characters);
+    this.characters.next(characters);
   }
   updateKatakana(characters: CharacterModel[]) {
     this.katakana.next(characters);
+    this.characters.next(characters);
   }
 }
--- src/app/study-kana/study-kana.component.ts
+++ src/app/study-kana/study-kana.component.ts
@@ -10,16 +10,25 @@ import { MdDialog } from '@angular/material';
   styleUrls: ['./study-kana.component.scss']
 })
 export class StudyKanaComponent implements OnInit {
-  private katakana: CharacterModel[];
-
+  private currentIndex = 0;
+  private showAnswer = false;
   constructor(private languageSelectService: LanguageSelectService, private dialog: MdDialog) { }
 
   ngOnInit() {
     this.configure();
   }
+
   configure() {
     this.dialog.closeAll();
     this.dialog.open(LanguageSelectComponent);
   }
 
+  click() {
+    if (this.showAnswer) {
+      this.currentIndex++;
+      this.showAnswer = false;
+    } else {
+      this.showAnswer = true;
+    }
+  }
 }
